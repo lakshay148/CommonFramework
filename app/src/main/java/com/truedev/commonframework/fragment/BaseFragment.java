aee9a1b41f1ec20bafea0eae65cec9378cb4716a
lakshay girdhar <lakshay148@gmail.com>
2016-10-24 19:20:47 +0530

    Default Colors added

--- app/src/main/java/com/truedev/commonframework/fragment/BaseFragment.java
+++ app/src/main/java/com/truedev/commonframework/fragment/BaseFragment.java
@@ -1,11 +1,12 @@
 package com.truedev.commonframework.fragment;
 
+import android.app.Fragment;
 /**
  * @author lakshaygirdhar
  * @version 1.0
  * @since 24/10/16
  */
 
-public class BaseFragment
+public class BaseFragment extends Fragment
 {
 }
